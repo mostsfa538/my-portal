79fc12aa829fe93330bef4ebbbcd0445aafd8997
LORDyyyyy <mlord6271@gmail.com>
2023-11-30 13:40:41 +0200

    dark mode

--- static/js/home.js
+++ static/js/home.js
@@ -40,3 +40,27 @@ function dateSuffix(date)
         default: return date + "th";
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('toggle-dark-mode');
+    const body = document.body;
+
+    const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+					document.getElementById('toggle-dark-mode').checked = true;;
+
+    }
+
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    });
+});
+
