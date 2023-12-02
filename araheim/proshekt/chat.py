bf15eae3afa79b68f9daf0b6c17c2ffbd1e25d42
LORDyyyyy <mlord6271@gmail.com>
2023-12-02 03:16:36 +0200

    general chat

--- araheim/proshekt/chat.py
+++ araheim/proshekt/chat.py
@@ -4,21 +4,21 @@ from flask_mysqldb import MySQL
 app = Flask(__name__, static_url_path='/static') 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'my_portal'
 mysql = MySQL(app)
 @app.route('/')
 def displayAnnouncementChat():
-    Names = mysql.connection.cursor()
-    Messages = mysql.connection.cursor()
-    Names.execute(f"SELECT first_name FROM  teacher INNER JOIN subjectchat ON teacher.id = subjectchat.messageSender where subject = {1}")
-    Messages.execute(f"SELECT message FROM subjectchat where subject = {1}")
-    namesData = [str(row).strip("(),'") for row in Names.fetchall()]
-    messagesData =[str(row).strip("(),'") for row in Messages.fetchall()]
-    Names.close()
-    Messages.close()
-    data_dict = [{'name': name, 'message': message} for name, message in zip(namesData, messagesData)]
-    return render_template('index.html', data = data_dict)
+    # Names = mysql.connection.cursor()
+    # Messages = mysql.connection.cursor()
+    # Names.execute(f"SELECT first_name FROM  teacher INNER JOIN subject_ann_chat ON teacher.id = subject_ann_chat.messageSender where subject = {1}")
+    # Messages.execute(f"SELECT message FROM subject_ann_chat where subject = {1}")
+    # namesData = [str(row).strip("(),'") for row in Names.fetchall()]
+    # messagesData =[str(row).strip("(),'") for row in Messages.fetchall()]
+    # Names.close()
+    # Messages.close()
+    # data_dict = [{'name': name, 'message': message} for name, message in zip(namesData, messagesData)]
+    return render_template('index.html', data = {})
 def displayGeneralChat():
     pass
 if __name__ == '__main__':
--- subject.py
+++ subject.py
@@ -1,6 +1,6 @@
 from DB_connect import mysql
 from DB_connect import app
-from flask import render_template, redirect, url_for, session, request, flash
+from flask import render_template, redirect, url_for, session, request, flash, jsonify
 
 
 @app.route("/subject/<string:code>", methods=['GET'])
@@ -13,4 +13,92 @@ def subject_page(code):
     id = session['id']
     submit = True
     role = session['role']
-    return render_template('subject.html', role=role)
+    return render_template('subject.html', role=role, code=code)
+
+
+@app.route("/subject/<string:code>/chat", methods=['GET'])
+def chat_page(code):
+    successful = ('email' in session)
+    if not successful:
+        return redirect('/login')
+    chat_messages = get_chat_messages(code)
+    first_name = ''
+    email = session['email']
+    id = session['id']
+    submit = True
+    role = session['role']
+    cur = mysql.connection.cursor()
+    cur.execute(f"SELECT profile_avatar FROM {role} WHERE id = %s", (id,))
+    result = cur.fetchone()
+    pfp = result[0]
+    cur.close()
+    return render_template('chat.html', role=role, code=code, chat_messages=chat_messages, pfp_link=pfp)
+
+
+@app.route('/subject/<string:code>/chat/api/send', methods=['POST'])
+def send_chat_message(code):
+    successful = ('email' in session)
+    if not successful:
+        return redirect('/login')
+    user_id = session['id']
+    role = session['role']
+    message = request.form.get('message')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id FROM subject WHERE code = %s", (code,))
+    result = cur.fetchone()
+    sub_id = result[0]
+    cur.execute("INSERT INTO chat_messages (sub_id, user_id, role, message) VALUES (%s, %s, %s, %s)",
+                (sub_id, user_id, role, message))
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify(success=True)
+
+
+@app.route('/subject/<string:code>/chat/api')
+def get_chat_messages(code):
+    successful = ('email' in session)
+    if not successful:
+        return redirect('/login')
+    chat_messages = get_chat_messages(code)
+    chat_messages = [list(t) for t in chat_messages]
+    user_id = session['id']
+    cur = mysql.connection.cursor()
+    for index, message in enumerate(chat_messages):
+        # print(session['id'], f'==>{message[0]}<===')
+        if str(message[0]) == str(session['id']):
+            chat_messages[index].append('right')
+            cur.execute(
+                f"SELECT profile_avatar, first_name FROM `{session['role']}` WHERE id = {user_id}")
+            result = cur.fetchone()
+            chat_messages[index].append(str(result[0]))
+            chat_messages[index].append(str(result[1]))
+            print(result)
+        else:
+            chat_messages[index].append('left')
+            cur.execute(
+                f"SELECT profile_avatar, first_name FROM `{message[3]}` WHERE id = %s", (message[0],))
+            result = cur.fetchone()
+            chat_messages[index].append(str(result[0]))
+            chat_messages[index].append(str(result[1]))
+            print('else:', message[0])
+
+    cur.execute(
+        f"SELECT profile_avatar FROM `{session['role']}` WHERE id = {user_id}")
+    result = cur.fetchone()
+    pfp = result[0]
+    print(chat_messages)
+    return render_template('chat_messages.html', chat_messages=chat_messages, pfp=pfp)
+
+
+def get_chat_messages(code):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id FROM subject WHERE code = %s", (code,))
+    result = cur.fetchone()
+    sub_id = result[0]
+    cur.execute(
+        "SELECT user_id, message, timestamp, role FROM chat_messages WHERE sub_id = %s ORDER BY timestamp ASC", (sub_id,))
+    chat_messages = cur.fetchall()
+    cur.close()
+    print(sub_id)
+    return (chat_messages)
